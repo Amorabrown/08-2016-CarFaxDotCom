cce10bcde97c46cce410d9b40a4350971bcbd758
Amora Brown <Amoraisnice@gmail.com>
2016-08-31 20:03:23 -0700

    worked with Elaine carFax

--- src/carfax.js
+++ src/carfax.js
@@ -2,3 +2,46 @@ var database = database.carDataBase
 var carData = database.carData;
 console.log(carData);
 // your code here
+
+toolbelt.loop([1,2,3,4], function (number){
+	console.log(number)
+})
+
+var result = toolbelt.transform([1,2,3,4], function (number){
+	return number * 2
+})
+console.log(result)
+
+
+var priceParser = function (collection){
+	return toolbelt.transform(collection, function (element){
+		return element.price
+	})
+}
+var result = priceParser(carData)
+
+database.currentPrices = result
+
+console.log(result)
+
+var yearParser = function(collection){
+	return toolbelt.transform (collection, function (element){
+		return element.year
+	})
+}
+var result = yearParser(carData)
+
+database.currentYears = result
+console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
